6e48fd2539d6473f8e3efef35da9ccff08a2b577
Ryan Sullivan <ryan@bossalien.com>
2016-04-20 14:19:52 +0100

    Implement Multiply in hack assembly

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -6,4 +6,31 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//make sure R2 is empty
+@R2
+M=0;
+
+(LOOP)
+//R1 is the counter if its at 0 we go to end
+@R1
+D=M
+@END
+D;JLE
+
+//decrement the counter R1 ready for the next iteration
+@R1
+M=M-1
+
+//increment the accumulator R2, by the value R0
+@R0
+D=M
+@R2
+M=D+M
+
+@LOOP
+0;JMP
+
+//infinite loop to avoid executing code inserted after the end of the program
+(END)
+@END
+0;JMP
